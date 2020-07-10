e44c92cb228db9514a47d71679b0e92ebba3c433
Brume <bumukoro@hotmail.com>
2020-07-10 14:42:17 +0100

    Update doc string and comment on mysql_arguments_example.py

--- mysql_arguments_example.py
+++ mysql_arguments_example.py
@@ -1,6 +1,7 @@
-""" mysql_arguments_git.py
+""" mysql_arguments_example.py
 
-A file to be filled in with the details for your mysql server and databases.
+A file to be filled in with the details for your mysql server and databases. Rename to mysql_arguments.py to use with
+the librar.
 """
 
 import urllib.parse
@@ -10,6 +11,6 @@ password = urllib.parse.quote_plus(password)
 host = ""
 port = ""
 
-# This is the table that you will store your daily adjusted stock price data in, mine is called "stocks" but you may call it 
-# whatever you like.
+# This is the database that you will store your daily adjusted stock price data in, mine is called "stocks" but you may
+# call it whatever you like.
 database = "stocks"  
